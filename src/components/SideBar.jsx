1e7c60835ba5bf58c28ffa2d964139393593e069
rodrigopfialho <rodrigo.pereira1@hotmail.com>
2024-03-19 21:35:17 -0300

    finalizando a estrutura do site

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,6 +1,7 @@
 import {PencilLine} from 'phosphor-react'
 
 import styles from "./Sidebar.module.css";
+import { Avatar } from './Avatar';
 
 export function Sidebar() {
   return (
@@ -11,7 +12,7 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
-        <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/39073227?v=4" alt="Foto de Perfil" />
+        <Avatar src="https://avatars.githubusercontent.com/u/39073227?v=4"/>  
 
         <strong>Rodrigo Pereira</strong>
         <span>Web developer</span>
